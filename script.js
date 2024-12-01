b0eb293da15cdb24636218880e73b9e1c74a6446
Hessen108 <hesperianyx@gmail.com>
2024-12-01 22:31:05 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -7,25 +7,24 @@ document.addEventListener("DOMContentLoaded", () => {
     const iframe = container.querySelector('iframe'); // iframe을 통해 구글 드라이브 음성 파일 재생
     const bubble = document.getElementById(`bubble${index}`);
     const text = document.getElementById(`text${index}`);
+    const profileImg = container.querySelector('.profile-img'); //프로필 이미지
 
     // 각 컨테이너에서 data 속성 가져오기
     const title = container.getAttribute('data-title');
     const content = container.getAttribute('data-content');
     const audioId = container.getAttribute('data-id'); // E열에서 추출된 구글 드라이브 ID
+    const profileImageURL = container.getAttribute('data-profile'); // G열에서 프로필 이미지 URL
+
+    // 프로필 이미지 설정
+    profileImg.style.backgroundImage = `url(${profileImageURL})`;
 
     playButton.addEventListener("click", () => {
       // 버튼이 재생 중일 때
-      if (iframe.style.display === 'none' || iframe.paused) {
-        iframe.style.display = 'block'; // iframe 표시
+      if (audio.paused) {
+        audio.play();
         iframe.src = `https://drive.google.com/file/d/${audioId}/preview`; // 구글 드라이브 음성 파일 링크 업데이트
 
         playButton.innerHTML = "<span>■</span>";  // 버튼을 '■'로 변경
-
-        // 구글 드라이브 제공 재생 버튼도 클릭
-        const googlePlayButton = iframe.contentWindow.document.querySelector('.goog-imageless-button'); // 구글 재생 버튼 찾기
-        if (googlePlayButton) {
-          googlePlayButton.click();  // 구글 재생 버튼 클릭
-        }
 
         // 말풍선 표시
         bubble.style.display = "flex";
@@ -43,7 +42,7 @@ document.addEventListener("DOMContentLoaded", () => {
           }
         }, 100);  // 100ms마다 한 글자씩 나타냄
       } else {
-        iframe.style.display = 'none';  // 음성 정지 시 iframe 숨김
+        audio.pause();
         playButton.innerHTML = "<span>▶</span>";  // 버튼을 '▶'로 변경
         bubble.style.display = "none";  // 음성 정지 시 말풍선 숨김
       }
