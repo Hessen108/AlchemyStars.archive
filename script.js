c2a60810003ccf8eef968d057604a512f515884b
Hessen108 <hesperianyx@gmail.com>
2024-12-02 01:10:35 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -4,7 +4,6 @@ document.addEventListener("DOMContentLoaded", () => {
 
   audioContainers.forEach((container, index) => {
     const playButton = container.querySelector('.custom-play-button');
-    const audio = container.querySelector('audio'); // audio 태그 선택
     const bubble = document.getElementById(`bubble${index}`);
     const text = document.getElementById(`text${index}`);
     const profileImg = container.querySelector('.profile-img'); // 프로필 이미지
@@ -13,33 +12,31 @@ document.addEventListener("DOMContentLoaded", () => {
     const title = container.getAttribute('data-title');
     const content = container.getAttribute('data-content');
     const audioId = container.getAttribute('data-id'); // E열에서 추출된 구글 드라이브 ID
-    const profileImageURL = container.getAttribute('data-profile'); // G열에서 프로필 이미지 URL
+    const profileImageID = container.getAttribute('data-profile'); // G열에서 전달된 프로필 이미지 ID
 
     // 프로필 이미지 설정
-    profileImg.style.backgroundImage = `url(${profileImageURL})`;
+    const imageViewLink = `https://drive.google.com/uc?export=view&id=${profileImageID}`;
+    if (profileImg) {
+      profileImg.style.backgroundImage = `url(${imageViewLink})`;
+    }
 
     playButton.addEventListener("click", () => {
-      // 오디오 재생 시작
-      if (audio.paused) {
-        audio.play();
-        playButton.innerHTML = "<span>■</span>"; // 버튼 변경 (정지 버튼)
-        bubble.style.display = "flex"; // 말풍선 표시
-        text.innerHTML = ""; // 텍스트 초기화
-        const script = content;  // 대사 내용 가져오기
-        let i = 0;
-        const interval = setInterval(() => {
-          if (i < script.length) {
-            text.innerHTML += script.charAt(i);  // 한 글자씩 출력
-            i++;
-          } else {
-            clearInterval(interval);  // 모든 글자가 출력되면 종료
-          }
-        }, 100);  // 100ms마다 한 글자씩 나타냄
-      } else {
-        audio.pause();  // 오디오 정지
-        playButton.innerHTML = "<span>▶</span>";  // 버튼을 '▶'로 변경
-        bubble.style.display = "none";  // 음성 정지 시 말풍선 숨김
-      }
+      const audio = new Audio(`https://drive.google.com/uc?export=download&id=${audioId}`);
+      audio.play();
+
+      playButton.innerHTML = "<span>■</span>"; // 버튼 변경
+      bubble.style.display = "flex";
+      text.innerHTML = ""; // 텍스트 초기화
+      const script = content;  // 대사 내용 가져오기
+      let i = 0;
+      const interval = setInterval(() => {
+        if (i < script.length) {
+          text.innerHTML += script.charAt(i);  // 한 글자씩 출력
+          i++;
+        } else {
+          clearInterval(interval);  // 모든 글자가 출력되면 종료
+        }
+      }, 100);  // 100ms마다 한 글자씩 나타냄
     });
   });
 });
